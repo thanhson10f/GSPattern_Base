450bea5a3964e6682eae094b04e939a3b4b6c595
thanhson10f <ntson.sh@gmail.com>
2013-11-14 10:26:35 +0700

    autofac + automapper

---
+++ GSPattern/GSP.Domain/Commands/Security/UserRegisterCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GSP.CommandProcessor.Command;
+
+namespace GSP.Domain.Commands.Security
+{
+    public class UserRegisterCommand : ICommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public int Status { get; set; }
+    }
+}
